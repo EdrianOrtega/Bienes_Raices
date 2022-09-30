442af8ca885908d08a1c139d74994a57859b2732
EdrianOrtega <inuyasha09889@gmail.com>
2022-09-29 22:30:59 -0500

    consultando la base de datos para obtener todas las propiedades

--- admin/index.php
+++ admin/index.php
@@ -1,22 +1,12 @@
 <?php 
 
-    require '../includes/funciones.php'; 
-    $auth = estaAutenticado(); 
- 
-     if(!$auth) {
-         header('Location: /'); 
-     }
+    require '../includes/app.php'; 
+    estaAutenticado(); 
 
+    use App\Propiedad; 
 
-    // Importar la conexión 
-    require '../includes/config/database.php'; 
-    $db = conectarDB(); 
-
-    // Escribir el query 
-    $query = "SELECT * FROM propiedades"; 
-
-    // Consultar la BD (Base de Datos) 
-    $resultadoConsulta = mysqli_query($db, $query); 
+    // Implementar un método para obtener todas las propiedades 
+    $propiedades = Propiedad::all(); 
 
     //Muestra mensaje condicionado 
     $resultado = $_GET['resultado'] ?? null; 
@@ -75,24 +65,24 @@
             </thead>
 
             <tbody> <!-- Mostrar los resultados --> 
-                <?php while( $propiedad = mysqli_fetch_assoc($resultadoConsulta)): ?> 
+                <?php foreach( $propiedades as $propiedad ): ?> 
                 <tr>
-                    <td> <?php echo $propiedad['id']; ?> </td> 
-                    <td> <?php echo $propiedad['titulo']; ?> </td>
-                    <td> <img src="/imagenes/<?php echo $propiedad['imagen']; ?>" class="imagen-tabla"> </td> 
-                    <td> <?php echo $propiedad['precio']; ?> </td>
+                    <td> <?php echo $propiedad->id; ?> </td> 
+                    <td> <?php echo $propiedad->titulo; ?> </td>
+                    <td> <img src="/imagenes/<?php echo $propiedad->imagen; ?>" class="imagen-tabla"> </td> 
+                    <td> <?php echo $propiedad->precio; ?> </td>
                     <td> 
                         <form method="POST" class="w-100"> 
 
-                            <input type="hidden" name="id" value="<?php echo $propiedad['id']; ?>"> 
+                            <input type="hidden" name="id" value="<?php echo $propiedad->id; ?>"> 
 
                             <input type="submit" class="boton-rojo-block" value="Eliminar"> 
                         </form>
 
-                        <a href="admin/propiedades/actualizar.php?id=<?php echo $propiedad['id']; ?>" class="boton-amarillo-block"> Actualizar </a> 
+                        <a href="admin/propiedades/actualizar.php?id=<?php echo $propiedad->id; ?>" class="boton-amarillo-block"> Actualizar </a> 
                     </td>
                 </tr>
-                <?php endwhile; ?> 
+                <?php endforeach; ?> 
             </tbody>
         </table> 
 
--- classes/Propiedad.php
+++ classes/Propiedad.php
@@ -127,6 +127,44 @@ class Propiedad {
         return self::$errores; 
     }
 
+    // Lista todas las propiedades 
+    public static function all() {
+        $query = "SELECT * FROM propiedades"; 
+
+        $resultado = self::consultarSQL($query);
+
+        return $resultado; 
+    }
+
+    public static function consultarSQL($query) {
+        // Consultar la base de datos 
+        $resultado = self::$db->query($query); 
+
+        // Iterar los resultados 
+        $array = []; 
+        while($registro = $resultado->fetch_assoc()) {
+            $array[] = self::crearObjeto($registro); 
+        }
+
+        // Liberar la memoria 
+        $resultado->free(); 
+
+        // Retornar los resultados 
+        return $array; 
+    }
+
+    protected static function crearObjeto($registro) {
+        $objeto = new self; 
+
+        foreach($registro as $key => $value ) {
+            if( property_exists( $objeto, $key ) ) {
+                $objeto->$key = $value; 
+            }
+        }
+
+        return $objeto; 
+    }
+
 
 }
 
